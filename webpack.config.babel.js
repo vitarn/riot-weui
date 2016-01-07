9e37d279b5aa5b5e165f842f1df44d7720ad0ea0
colder <cncolder@gmail.com>
2016-01-07 16:33:26 +0800

    npm build

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -2,15 +2,14 @@ import path from 'path'
 import webpack from 'webpack'
 import OpenBrowserPlugin from 'open-browser-webpack-plugin'
 
+const PROD = 'production' == process.env.NODE_ENV
 const resolve = p => path.resolve(__dirname, p)
 
 export default {
   entry: [
-    // 'webpack/hot/dev-server?reload=true',
-    // 'webpack-dev-server/client?http://localhost:8080',
     './example/index.js',
   ],
-  devtool: 'inline-source-map',
+  devtool: PROD ? [] : 'inline-source-map',
   output: {
     path: './example',
     publicPath: '/',
@@ -23,7 +22,8 @@ export default {
       loader: resolve('./utils/riotjs-loader'),
       query: {
         type: 'babel',
-        debug: true,
+        debug: !PROD,
+        compact: PROD,
       },
     }, ],
     loaders: [{
@@ -43,11 +43,27 @@ export default {
       loader: 'style!css'
     }, ],
   },
-  plugins: [
+  plugins: PROD ? [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
     new webpack.ProvidePlugin({
       riot: 'riot',
     }),
-    // new webpack.HotModuleReplacementPlugin(),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      sourceMap: false,
+      compressor: {
+        warnings: false,
+      },
+    }),
+  ] : [
+    new webpack.ProvidePlugin({
+      riot: 'riot',
+    }),
     new webpack.NoErrorsPlugin(),
     new OpenBrowserPlugin({
       url: 'http://localhost:8080'
